626e9b21f59ce45ad403c485066f5963437b3a9c
Kristen Van Etten <kristenvanetten@yahoo.com>
2020-07-18 23:28:24 -0400

    Updated JS
    Cross browser and animations fixes

--- javascript/main.js
+++ javascript/main.js
@@ -1,137 +1,72 @@
-////////Play Animation Button////////////////
+////////////////////////////////////////////////////////////////////
+/////////////////Sticky header//////////////////////////
+////////////////////////////////////////////////////////////////////
 
-$(document).ready(() => {
-  $('.animation-trigger').on('click', () => {
-      $('.toggle-animation').toggleClass('animation-on');
-      $('.toggle-animation').toggleClass('hide-element');
-      
-   // Change Text of button
-        if( $('.text-swap').hasClass('animation-on') ) {
-          $('.animation-trigger').text('Hide Animation');
-        }
-        else {
-        $('.animation-trigger').text('Play Animation');
-        }
+    // When the user scrolls the page, execute myFunction 
+    window.onscroll = function() {myFunction();};  
 
-    // [this doens't work] if(document.getElementByID('a-background').hasClass('animation-on') ) {
-    //       $('.animation-trigger').text('Hide Animation');
-    //     }
-    //     else {
-    //     $('.animation-trigger').text('Play Animation');
-    //     }
+    // Get the header
+    var navbar = document.getElementById("my-navbar");
 
-    
-    });
-});
+    // Get the offset position of the navbar
+    var sticky = navbar.offsetTop;
 
-
-////////Sticky header/////////////////
-
-  // When the user scrolls the page, execute myFunction 
-  window.onscroll = function() {myFunction();};  
-
-  // Get the header
-  var navbar = document.getElementById("my-navbar");
-
-  // Get the offset position of the navbar
-  var sticky = navbar.offsetTop;
-
-  // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-  function myFunction() {
-    if (window.pageYOffset >= sticky) {
-      navbar.classList.add("sticky-nav");
-      $('.anchor').addClass('offset1');
-      
-    } else {
-      navbar.classList.remove("sticky-nav");
-      $('.anchor').removeClass('offset1');
+    // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function myFunction() {
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky-nav");
+        $('.anchor').addClass('offset1');
+        
+      } else {
+        navbar.classList.remove("sticky-nav");
+        $('.anchor').removeClass('offset1');
+      }
     }
-  }
 
+////////////////////////////////////////////////////////////////////
+////////////////////Smooth Scroll on Anchor Links//////////////////
+////////////////////////////////////////////////////////////////////
 
-///////////Smooth Scroll on Anchor Links//////////////////////////
-$('.anchor').on('click', function(e) {
-  e.preventDefault();
+    $('.anchor').on('click', function(e) {
+      e.preventDefault();
 
-  //Offset for when scrolled down and sticky nav in place //
-  if ($("a").hasClass("anchor offset1")) {
+      //Offset for when scrolled down and sticky nav in place //
+      if ($("a").hasClass("anchor offset1")) {
 
-      // alert("Hello! If");
-      $('html, body').animate(
-        {
-          scrollTop: $($(this).attr('href')).offset().top -18,
-        },
-        500,
-        'linear'
-      );
-  } 
+          // alert("Hello! If");
+          $('html, body').animate(
+            {
+              scrollTop: $($(this).attr('href')).offset().top -18,
+            },
+            500,
+            'linear'
+          );
+      } 
 
-  else {
+      else {
+          // Offset for when starting at top;
+            $('html, body').animate(
+              {
+                scrollTop: $($(this).attr('href')).offset().top -76,
+              },
+              500,
+              'linear'
+            );
+      }
+    });
 
-      // Offset for when starting at top;
+///////////Smooth Scroll on Anchor2 Links//////////////////////////
+    $('.anchor2').on('click', function(e) {
+      e.preventDefault();
+
         $('html, body').animate(
           {
-            scrollTop: $($(this).attr('href')).offset().top -76,
+            scrollTop: $($(this).attr('href')).offset().top,
           },
           500,
           'linear'
         );
-  }
-
-});
-
-
-///////////Smooth Scroll on Anchor2 Links//////////////////////////
-$('.anchor2').on('click', function(e) {
-  e.preventDefault();
-
-    $('html, body').animate(
-      {
-        scrollTop: $($(this).attr('href')).offset().top,
-      },
-      500,
-      'linear'
-    );
-
-});
-
-
-////////////////////////////////////////////////////////////////////
-////////////////Mobile Menu Icon/////////////////////
-////////////////////////////////////////////////////////////////////
-
-$(document).ready(() => {
-  
-  $('.m-dropdown-trigger').on('click', () => {
-    $('.menu-container').toggleClass('mobile');
-    $('.dropdown').toggleClass('mobile');
-    $('.dropdown-content').toggleClass('mobile');
-
-    $('.menu-container mobile').slideToggle();
-  });
-
-  $('.close-trigger').on('click', () => {
-    $('.menu-container').toggleClass('mobile');
-    $('.dropdown').toggleClass('mobile');
-    $('.dropdown-content').toggleClass('mobile');
-
-    $('.menu-container mobile').slideToggle();
-  });
-
-  $('.b-trigger').on('click', () => {
-    $('.blog-return').toggleClass('hide-blog');
-  });
-
-  //   $('.mobile-dropdown-content').on('click', () => {
-  //   $('.mobile-dropdown-content').fadeToggle();
-  // });
-
-  //   $('.dropdown-content').on('click', () => {
-  //   $('.dropdown-content').slideToggle();
-  // });
-
-  
-});
+    });
 
 ////////////////////////////////////////////////////////////////////
 ////////////////GIF Play on Rollover/////////////////////
@@ -152,19 +87,72 @@ $(document).ready(function()
         });
 
 });
+////////////////////////////////////////////////////////////////////
+////////////////Mobile Menu Icon/////////////////////
+////////////////////////////////////////////////////////////////////
 
+$(document).ready(function() 
+{
+  
+  $('.m-dropdown-trigger').on('click', function() {
+    $('.menu-container').toggleClass('mobile');
+    $('.dropdown').toggleClass('mobile');
+    $('.dropdown-content').toggleClass('mobile');
 
+    $('.menu-container mobile').slideToggle();
+  });
 
+  $('.close-trigger').on('click', function() {
+    $('.menu-container').toggleClass('mobile');
+    $('.dropdown').toggleClass('mobile');
+    $('.dropdown-content').toggleClass('mobile');
 
+    $('.menu-container mobile').slideToggle();
+  });
 
+  $('.b-trigger').on('click', function() {
+    $('.blog-return').toggleClass('hide-blog');
+  });
+
+  //   $('.mobile-dropdown-content').on('click', function() {
+  //   $('.mobile-dropdown-content').fadeToggle();
+  // });
+
+  //   $('.dropdown-content').on('click', function() {
+  //   $('.dropdown-content').slideToggle();
+  // });
+
+  
+});
+
+////////////////////////////////////////////////////////////////////
+////////Play Animation Button////////////////
+////////////////////////////////////////////////////////////////////
+
+$(document).ready(function() {
+
+  $('.animation-trigger').on('click', function() {
+      $('.toggle-animation').toggleClass('animation-on');
+      $('.toggle-animation').toggleClass('hide-element');
+      
+   // Change Text of button
+        if( $('.text-swap').hasClass('animation-on') ) {
+          $('.animation-trigger').text('Hide Animation');
+        }
+        else {
+        $('.animation-trigger').text('Play Animation');
+        }    
+    });
+
+});
 
 ////////////////////////////////////////////////////////////////////
 ////////////////Slideshow/////////////////////
 ////////////////////////////////////////////////////////////////////
 
-$(document).ready(() => {
+$(document).ready(function() {
   
-  $('.btn1').on('click', () => {    
+  $('.btn1').on('click', function() {    
     $('.slide1').slideToggle(600);
     $('.slide2').slideUp();
     $('.slide3').slideUp();
@@ -179,7 +167,7 @@ $(document).ready(() => {
     $('.btn6').removeClass('inactive');
   });
 
-    $('.btn2').on('click', () => {
+    $('.btn2').on('click', function() {
     $('.slide2').slideToggle(600);
     $('.slide1').slideUp();
     $('.slide3').slideUp();
@@ -194,7 +182,7 @@ $(document).ready(() => {
     $('.btn6').removeClass('inactive');
   });
 
-    $('.btn3').on('click', () => {
+    $('.btn3').on('click', function() {
     $('.slide3').slideToggle(600);
     $('.slide1').slideUp();
     $('.slide2').slideUp();
@@ -209,7 +197,7 @@ $(document).ready(() => {
     $('.btn6').removeClass('inactive'); 
   });
 
-    $('.btn4').on('click', () => {
+    $('.btn4').on('click', function() {
     $('.slide4').slideToggle(600);
     $('.slide1').slideUp();
     $('.slide2').slideUp();
@@ -224,7 +212,7 @@ $(document).ready(() => {
     $('.btn6').removeClass('inactive');  
   });
 
-    $('.btn5').on('click', () => {
+    $('.btn5').on('click', function() {
     $('.slide5').slideToggle(600);
     $('.slide1').slideUp();
     $('.slide2').slideUp();
@@ -239,7 +227,7 @@ $(document).ready(() => {
     $('.btn6').removeClass('inactive');
   });
 
-    $('.btn6').on('click', () => {
+    $('.btn6').on('click', function() {
     $('.slide6').slideToggle(600);
     $('.slide1').slideUp();
     $('.slide2').slideUp();
@@ -254,20 +242,108 @@ $(document).ready(() => {
     $('.btn6').toggleClass('inactive');  
   });
 
+  $('.0-back').on('click', function() {
+    $('.slide6').slideDown(600);
+    $('.btn6').addClass('inactive');
+  });
 
+  $('.0-next').on('click', function() {
+    $('.slide1').slideDown(600);
+    $('.btn1').addClass('inactive');
+  });
 
+  $('.1-back').on('click', function() {
+    $('.slide1').slideUp();
+    $('.btn1').removeClass('inactive');
+  });
+
+  $('.1-next').on('click', function() {
+    $('.slide1').slideUp();
+    $('.slide2').slideDown(600);
+    $('.btn1').removeClass('inactive');
+    $('.btn2').addClass('inactive'); 
+  });
+
+  $('.2-back').on('click', function() {
+    $('.slide2').slideUp();
+    $('.slide1').slideDown(600);
+    $('.btn2').removeClass('inactive');
+    $('.btn1').addClass('inactive'); 
+  });
+
+  $('.2-next').on('click', function() {
+    $('.slide2').slideUp();
+    $('.slide3').slideDown(600);
+    $('.btn2').removeClass('inactive');
+    $('.btn3').addClass('inactive'); 
+  });
+
+  $('.3-back').on('click', function() {
+    $('.slide3').slideUp();
+    $('.slide2').slideDown(600);
+    $('.btn3').removeClass('inactive');
+    $('.btn2').addClass('inactive'); 
+  });
+
+  $('.3-next').on('click', function() {
+    $('.slide3').slideUp();
+    $('.slide4').slideDown(600);
+    $('.btn3').removeClass('inactive');
+    $('.btn4').addClass('inactive'); 
+  });
+
+  $('.4-back').on('click', function() {
+    $('.slide4').slideUp();
+    $('.slide3').slideDown(600);
+    $('.btn4').removeClass('inactive');
+    $('.btn3').addClass('inactive'); 
+  });
+
+  $('.4-next').on('click', function() {
+    $('.slide4').slideUp();
+    $('.slide5').slideDown(600);
+    $('.btn4').removeClass('inactive');
+    $('.btn5').addClass('inactive'); 
+  });      
+
+  $('.5-back').on('click', function() {
+    $('.slide5').slideUp();
+    $('.slide4').slideDown(600);
+    $('.btn5').removeClass('inactive');
+    $('.btn4').addClass('inactive'); 
+  });
+
+  $('.5-next').on('click', function() {
+    $('.slide5').slideUp();
+    $('.slide6').slideDown(600);
+    $('.btn5').removeClass('inactive'); 
+    $('.btn6').addClass('inactive');
+  });  
+
+  $('.6-back').on('click', function() {
+    $('.slide6').slideUp();
+    $('.slide5').slideDown(600);
+    $('.btn6').removeClass('inactive');
+    $('.btn5').addClass('inactive'); 
+  });
+
+  $('.6-next').on('click', function() {
+    $('.slide6').slideUp();
+    $('.btn6').removeClass('inactive');
+  });    
  }); 
+
 
 ////////////////////////////////////////////////////////////////////
 ////////////////Contact Pop Up/////////////////////
 ////////////////////////////////////////////////////////////////////
 
-// $(document).ready(() => {
-//   $('.contact-me').on('click', () => {
+// $(document).ready(function() {
+//   $('.contact-me').on('click', function() {
 //       $('.contact-form').slideToggle();
 //     });
 
-//   $('.close-contact').on('click', () => {
+//   $('.close-contact').on('click', function() {
 //       $('.contact-form').slideToggle();
 //     });
 
